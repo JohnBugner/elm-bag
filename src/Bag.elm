8a911b8e4e3fdf65f9888f2cbb6218cfb040dd09
John Bugner <john.bugner@gmail.com>
2017-06-12 07:24:32 -0500

    Added 'Bags' section.

--- src/Bag.elm
+++ src/Bag.elm
@@ -1,1 +1,17 @@
-module Bag exposing (..)
+module Bag exposing (Bag)
+
+{-| A set of values where, unlike `Set`, each value can appear multiple times.
+
+It's basically just a fancy wrapper for `Dict`, so like `Dict`,
+insert, remove, and query operations all take *O(log n)* time.
+
+# Bags
+@docs Bag
+-}
+
+import Dict exposing (Dict)
+
+{-| A set of possibly multiple values.
+-}
+type Bag a
+    = Bag (Dict a Int)
