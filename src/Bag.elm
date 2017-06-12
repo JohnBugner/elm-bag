e3b0dff1f3aa7a376c970e36f2ad17a66471f83f
John Bugner <john.bugner@gmail.com>
2017-06-12 09:12:13 -0500

    Added 'Build' section.

--- src/Bag.elm
+++ src/Bag.elm
@@ -1,5 +1,6 @@
 module Bag exposing
     ( Bag
+    , empty, repeat, insert, remove
     , isEmpty, member, count, size
     )
 
@@ -11,6 +12,9 @@ insert, remove, and query operations all take *O(log n)* time.
 # Bags
 @docs Bag
 
+# Build
+@docs empty, repeat, insert, remove
+
 # Query
 @docs isEmpty, member, count, size
 -}
@@ -21,6 +25,37 @@ import Dict exposing (Dict)
 -}
 type Bag a
     = Bag (Dict a Int)
+
+dict : Bag a -> Dict a Int
+dict (Bag d) = d
+
+{-| Create an empty bag.
+-}
+empty : Bag a
+empty = Bag Dict.empty
+
+{-| Create a bag with n copies of a value.
+-}
+repeat : comparable -> Int -> Bag comparable
+repeat value amount = Bag <| Dict.singleton value amount
+
+{-| Insert n copies of a value into a bag.
+-}
+insert : comparable -> Int -> Bag comparable -> Bag comparable
+insert value amount bag = Bag <| Dict.insert value (count value bag + amount) (dict bag)
+
+{-| Remove n copies of a value from a bag.
+If n is greater than the numbers of copies that are in the bag, then all copies are simply removed.
+
+    bag = fromList ['a', 'a', 'b']
+
+    remove 'a' 3 bag == fromList ['b']
+-}
+remove : comparable -> Int -> Bag comparable -> Bag comparable
+remove value amount bag =
+    if count value bag - amount <= 0
+    then Bag <| Dict.remove value (dict bag)
+    else Bag <| Dict.insert value (count value bag - amount) (dict bag)
 
 {-| Determine if a bag is empty.
 -}
