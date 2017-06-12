f7605d340cdbbff125afe2d44dc1fa9999186d17
John Bugner <john.bugner@gmail.com>
2017-06-12 10:14:27 -0500

    Added 'Combine' section.

--- src/Bag.elm
+++ src/Bag.elm
@@ -2,6 +2,7 @@ module Bag exposing
     ( Bag
     , empty, repeat, insert, remove
     , isEmpty, member, count, size
+    , union, intersect, diff
     )
 
 {-| A set of values where, unlike `Set`, each value can appear multiple times.
@@ -17,6 +18,9 @@ insert, remove, and query operations all take *O(log n)* time.
 
 # Query
 @docs isEmpty, member, count, size
+
+# Combine
+@docs union, intersect, diff
 -}
 
 import Dict exposing (Dict)
@@ -76,3 +80,45 @@ count v b = Maybe.withDefault 0 <| Dict.get v (dict b)
 -}
 size : Bag a -> Int
 size b = Dict.size (dict b)
+
+{-| Get the union of two sets. For a value, its two counts are added.
+-}
+union : Bag comparable -> Bag comparable -> Bag comparable
+union b1 b2 =
+    let
+        one : comparable -> Int -> Dict comparable Int -> Dict comparable Int
+        one v n d = Dict.insert v n d
+        both : comparable -> Int -> Int -> Dict comparable Int -> Dict comparable Int
+        both v n1 n2 d = Dict.insert v (n1 + n2) d
+    in
+        Bag <| Dict.merge one both one (dict b1) (dict b2) Dict.empty
+
+{-| Get the intersection of two sets. For a value, the lesser of its two counts is taken.
+-}
+intersect : Bag comparable -> Bag comparable -> Bag comparable
+intersect b1 b2 =
+    let
+        one : comparable -> Int -> Dict comparable Int -> Dict comparable Int
+        one _ _ d = d
+        both : comparable -> Int -> Int -> Dict comparable Int -> Dict comparable Int
+        both v n1 n2 d = Dict.insert v (min n1 n2) d
+    in
+        Bag <| Dict.merge one both one (dict b1) (dict b2) Dict.empty
+
+{-| Get the difference between of two sets.
+For a value, the count of the second is removed from the count of the first.
+-}
+diff : Bag comparable -> Bag comparable -> Bag comparable
+diff b1 b2 =
+    let
+        left : comparable -> Int -> Dict comparable Int -> Dict comparable Int
+        left v n d = Dict.insert v n d
+        both : comparable -> Int -> Int -> Dict comparable Int -> Dict comparable Int
+        both v n1 n2 d =
+            if n1 - n2 <= 0
+            then d
+            else Dict.insert v (n1 - n2) d
+        right : comparable -> Int -> Dict comparable Int -> Dict comparable Int
+        right _ _ d = d
+    in
+        Bag <| Dict.merge left both right (dict b1) (dict b2) Dict.empty
